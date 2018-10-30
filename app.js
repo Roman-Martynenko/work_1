6d52869ad1730327da4ece1eb0d7198536c49f45
Roman M <martynenko.roman.06@gmail.com>
2018-10-31 00:12:32 +0300

    Работа с node.js, express и post запросы

---
+++ app.js
@@ -0,0 +1,24 @@
+﻿
+//###### Подключаемые модули ######
+var express = require('express') // Модуль поднятия сервера
+var bodyParser = require("body-parser"); // Модуль для обработки пост запроссов
+//#################################
+
+var app = express();
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true }));
+
+//Подключаем Html форму при помощи метода res.sendfile('index.html')
+app.get('/', function (req, res) {
+    res.sendfile('index.html');
+});
+
+
+app.listen('16515', function () { console.log("server rabotaet") }) //этой командай запускаем node js сервер (3000 порт доступа)
+
+app.post('/page_test', function (req, res) {
+    res.send('Ответ от сервера на нажатие кнопки');
+    console.log(req.body);
+})
